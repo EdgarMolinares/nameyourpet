822d57da3d3a697f8c7562dd3fd5f68fea6808e8
EdgarMolinares <77982025+EdgarMolinares@users.noreply.github.com>
2021-01-26 16:36:01 -0600

    Update footer.php

--- html/footer.php
+++ html/footer.php
@@ -1,3 +1,7 @@
+<footer style="background:black">
+  <p>Creators: Jake Thorson, Edgar Molinares<br>
+  <a href="mailto:JThorson@edgewood.edu">JThorson@edgewood.edu</a></p>
+  <a href="mailto:Emolinares@edgewood.edu">Emolinares@edgewood.edu<a/>
 </div>
     </body>
 </html>
